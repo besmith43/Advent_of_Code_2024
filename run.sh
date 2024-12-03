07de222f0548e67324a2660520723621abe72131
Blake Smith <besmith43@gmail.com>
2024-12-03 14:53:34 -0600

    saving progress

--- run.sh
+++ run.sh
@@ -3,25 +3,8 @@
 
 figlet "Advent of Code 2024"
 
-# echo "running Day 1 Part 1"
-# ./day_1/pt1/main.csx
+day=3
+part=2
 
-
-# echo "running Day 1 Part 2"
-# ./day_1/pt2/main.csx
-
-
-# echo "running Day 2 Part 1"
-# ./day_2/pt1/main.csx
-
-
-# echo "running Day 2 Part 2"
-# ./day_2/pt2/main.csx
-
-
-# echo "running Day 3 Part 1"
-# ./day_3/pt1/main.csx
-
-
-echo "running Day 3 Part 2"
-./day_3/pt2/main.csx
+echo "running Day $day Part $part"
+./day_$day/pt$part/main.csx
